54d040b6900ec2df009581de3c12463c0581c954
Bradley Davis <me@bradleydavis.tech>
2024-02-08 08:30:05 -0800

    Added animations on button press

--- main.cpp
+++ main.cpp
@@ -1,41 +1,89 @@
+#include <stdint.h>
 #include <stdio.h>
 
 #include <Adafruit_NeoPixel.hpp>
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+#include "pico/time.h"
+#include "pico/types.h"
 
-#include "hardware/adc.h"
-
-constexpr size_t LED_PIN          = 25;
-constexpr size_t TEMP_ADC         = 4;
-constexpr size_t NEOPIXEL_LED_PIN = 0;
+constexpr uint PIN_LED      = 25;
+constexpr uint PIN_NEOPIXEL = 0;
+constexpr uint PIN_BTN      = 1;
 
 constexpr size_t LED_COUNT_BTN    = 12;
 constexpr size_t LED_COUNT_MIRROR = 24;
 constexpr size_t LED_COUNT        = LED_COUNT_MIRROR + LED_COUNT_BTN;
 
-constexpr size_t BRIGHTNESS = 10;
+constexpr uint8_t BRIGHTNESS = 10;
 
-constexpr float conversion_factor = 3.3f / (1 << 12);
+constexpr float FRAME_RATE            = 20.0f;
+constexpr uint64_t FRAME_PERIOD_US    = 1e6 / FRAME_RATE;
+constexpr uint64_t DURATION_RING_US   = 5e6;
+constexpr uint64_t DURATION_NOTIFY_US = 15e6;
 
-Adafruit_NeoPixel strip(LED_COUNT, NEOPIXEL_LED_PIN, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip(LED_COUNT, PIN_NEOPIXEL, NEO_GRBW + NEO_KHZ800);
 
-void colorWipe(uint32_t color, int wait);
-void rainbow(int wait);
+// Offset indices since the strips are daisy-chained
+#define setColorBtn(i, c) \
+    { strip.setPixelColor(LED_COUNT_MIRROR + i, c); }
+#define setColorMirror(i, c) \
+    { strip.setPixelColor(i, c); }
+
+uint32_t WHITE = strip.Color(0, 0, 0, 255);
+uint32_t colorWheel(uint16_t degrees);
+
+// Animation functions
+typedef void (*animation_t)(uint16_t);
+
+void rainbow(uint16_t frame);
+void theatreChase(uint16_t frame);
+void theatreChaseRainbow(uint16_t frame);
+void pulseWhite(uint16_t frame);
+
+constexpr animation_t ANIMATIONS[] = {
+    rainbow,
+    theatreChase,
+    theatreChaseRainbow,
+    pulseWhite,
+};
+constexpr size_t N_ANIMATIONS = 4;
+
+enum class State : uint8_t {
+    IDLE   = 0,
+    RING   = 1,
+    NOTIFY = 2,
+};
 
 int main() {
     stdio_init_all();
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    bool act_led_on = false;
+    gpio_init(PIN_LED);
+    gpio_set_dir(PIN_LED, GPIO_OUT);
+    gpio_put(PIN_LED, act_led_on);
 
-    adc_init();
-    adc_gpio_init(26);
-    adc_set_temp_sensor_enabled(true);
-
-    adc_select_input(TEMP_ADC);
+    bool btn_pressed_latched = false;
+    gpio_init(PIN_BTN);
+    gpio_set_dir(PIN_BTN, GPIO_IN);
+    gpio_set_pulls(PIN_BTN, true, false);
 
     strip.begin();
     strip.show();
     strip.setBrightness(BRIGHTNESS);
+
+    // Sleep for a bit to stablize pull-ups
+    sleep_ms(100);
+
+    uint32_t idle_color = strip.Color(0, 0, 0);
+    State state         = State::IDLE;
+
+    uint64_t now          = time_us_64();
+    uint64_t next_frame   = now;
+    uint64_t next_timeout = 0;
+
+    uint16_t current_frame   = 0;
+    size_t current_animation = 0;
 
     while (true) {
         // Default is no lights at all
@@ -44,39 +92,134 @@ int main() {
         // #RGB<cr> will change button to that color
         // !<cr> will start a notification on the mirror
 
-        gpio_put(LED_PIN, true);
-        sleep_ms(1000);
+        // Keep a consistant frame rate
+        now = time_us_64();
+        if (now < next_frame) {
+            // No interrupts so poll button if not ringing
+            if (state != State::RING && gpio_get(PIN_BTN) == 0) {
+                btn_pressed_latched = true;
+            }
+            sleep_us(100);
+            continue;
+        }
+        next_frame += FRAME_PERIOD_US;
 
-        colorWipe(strip.Color(255, 0, 0), 50);
-        colorWipe(strip.Color(0, 0, 0, 255), 50);
+        gpio_put(PIN_LED, act_led_on);
+        act_led_on = !act_led_on;
 
-        gpio_put(LED_PIN, false);
-        sleep_ms(1000);
+        if (btn_pressed_latched) {
+            // Actually handle button by starting ring animation
+            state               = State::RING;
+            current_frame       = 0;
+            btn_pressed_latched = false;
+            next_timeout        = now + DURATION_RING_US;
+            current_animation   = (current_animation + 1) % N_ANIMATIONS;
+        } else if (now > next_timeout) {
+            // Once timeout expires, go back to idle
+            state = State::IDLE;
+        }
 
-        rainbow(10);
-
-        const float voltage     = adc_read() * conversion_factor;
-        const float temperature = 27 - (voltage - 0.706) / 0.001721;
-
-        printf("Hello, world! The temperature is: %fc\n", temperature);
-    }
-}
-
-void colorWipe(uint32_t color, int wait) {
-    for (int i = 0; i < strip.numPixels(); ++i) {
-        strip.setPixelColor(i, color);
-        strip.show();
-        sleep_ms(wait);
-    }
-}
-
-void rainbow(int wait) {
-    for (long firstHue = 0; firstHue < 65536; firstHue += 256) {
-        for (int i = 0; i < strip.numPixels(); ++i) {
-            int pixelHue = firstHue + (i * 65536L / strip.numPixels());
-            strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+        switch (state) {
+            case State::IDLE:
+                // Set idle color on btn
+                for (uint16_t i = 0; i < LED_COUNT_BTN; ++i) {
+                    setColorBtn(i, idle_color);
+                }
+                // Turn mirror off
+                for (uint16_t i = 0; i < LED_COUNT_MIRROR; ++i) {
+                    setColorMirror(i, 0);
+                }
+                break;
+            case State::RING:
+                // Set white on btn
+                for (uint16_t i = 0; i < LED_COUNT_BTN; ++i) {
+                    setColorBtn(i, strip.Color(0, 0, 0, 255));
+                }
+                // Run animation
+                ANIMATIONS[current_animation](current_frame);
+                break;
+            case State::NOTIFY:
+                // Set idle color on btn
+                for (uint16_t i = 0; i < LED_COUNT_BTN; ++i) {
+                    setColorBtn(i, idle_color);
+                }
+                break;
         }
         strip.show();
-        sleep_ms(wait);
+
+        ++current_frame;
+    }
+}
+
+/**
+ * @brief Spin a rainbow color wheel around
+ *
+ * @param frame index of animation frame
+ */
+void rainbow(uint16_t frame) {
+    uint32_t firstHue = frame * 0x0400;
+    for (int i = 0; i < LED_COUNT_MIRROR; ++i) {
+        int pixelHue = firstHue + (i * 65536L / LED_COUNT_MIRROR);
+        setColorMirror(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+}
+
+/**
+ * @brief Theatre-marquee-style chasing lights
+ *
+ * @param frame index of animation frame
+ */
+void theatreChase(uint16_t frame) {
+    frame = frame % 3;
+    for (int i = 0; i < LED_COUNT_MIRROR; ++i) {
+        if ((i % 3) == frame) {
+            setColorMirror(i, WHITE);
+        } else {
+            setColorMirror(i, 0);
+        }
+    }
+}
+
+/**
+ * @brief Theatre-marquee-style chasing lights
+ *
+ * @param frame index of animation frame
+ */
+void theatreChaseRainbow(uint16_t frame) {
+    uint32_t firstHue = frame * 0x0400;
+    frame             = frame % 3;
+
+    for (int i = 0; i < LED_COUNT_MIRROR; ++i) {
+        if ((i % 3) == frame) {
+            int pixelHue = firstHue + (i * 65536L / LED_COUNT_MIRROR);
+            setColorMirror(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+        } else {
+            setColorMirror(i, 0);
+        }
+    }
+}
+
+/**
+ * @brief Pulse white white
+ *
+ * @param frame index of animation frame
+ */
+void pulseWhite(uint16_t frame) {
+    constexpr uint16_t N_UP   = FRAME_RATE / 2;
+    constexpr uint16_t N_DOWN = FRAME_RATE;
+    constexpr uint16_t N_LOOP = N_UP + N_DOWN;
+    frame                     = frame % N_LOOP;
+
+    constexpr uint8_t STEP_UP   = 255 / N_UP;
+    constexpr uint8_t STEP_DOWN = 255 / N_DOWN;
+
+    uint8_t b = STEP_UP * frame;
+    if (frame >= N_UP) {
+        frame -= N_UP;
+        b = 255 - (STEP_DOWN * frame);
+    }
+    uint32_t c = strip.Color(0, 0, 0, strip.gamma8(b));
+    for (int i = 0; i < LED_COUNT_MIRROR; ++i) {
+        setColorMirror(i, c);
     }
 }
